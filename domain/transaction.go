76903a24a5baea4783ffa6f7cbe7650098608735
Your Name <you@example.com>
2025-01-23 18:05:29 +0000

    fix: fixing domain Products and Transactions

--- domain/transaction.go
+++ domain/transaction.go
@@ -2,15 +2,14 @@ package domain
 
 import "time"
 
-type Product struct {
-    ProductID   uint      `gorm:"column:product_id;primaryKey;autoIncrement"`
-    ProductName string    `gorm:"column:product_name;type:TEXT;not null"`
-    Description string    `gorm:"column:description;type:TEXT"`
-    Price       float64   `gorm:"column:price;type:DECIMAL(10,2);not null;default:0"`
-    CreatedAt   time.Time `gorm:"column:created_at;not null;default:CURRENT_TIMESTAMP"`
-    UpdatedAt   time.Time `gorm:"column:updated_at;not null;default:CURRENT_TIMESTAMP"`
+type Transaction struct {
+    TransactionID   uint      `gorm:"column:transaction_id;primaryKey;autoIncrement"`
+    UserID          uint      `gorm:"column:user_id;not null"`
+    ProductID       uint      `gorm:"column:product_id;not null"`
+    TransactionValue float64  `gorm:"column:transaction_value;type:DECIMAL(10,2);not null;default:0"`
+    TransactionDate time.Time `gorm:"column:transaction_date;not null;default:CURRENT_TIMESTAMP"`
 }
 
-func (Product) TableName() string {
-    return "products"
+func (Transaction) TableName() string {
+    return "transactions"
 }

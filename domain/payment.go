f93c8339140da5f4852d67310911d93604a6c3cb
Your Name <you@example.com>
2025-01-27 18:33:56 +0000

    feat: foreignKey settings

--- domain/payment.go
+++ domain/payment.go
@@ -3,14 +3,21 @@ package domain
 import "time"
 
 type Payment struct {
-    PaymentID          uint      `gorm:"column:payment_id;primaryKey;autoIncrement"`
-    UserID             uint      `gorm:"column:user_id;not null"`
-    ProductID          *uint     `gorm:"column:product_id"` // pode ser nulo
-    ExternalPaymentID  string    `gorm:"column:external_payment_id;type:TEXT;not null"`
-    SupplierName       string    `gorm:"column:supplier_name;type:TEXT;not null"`
-    TopUpValue         float64   `gorm:"column:top_up_value;type:DECIMAL(10,2);not null;default:0"`
-    BalanceAfterTopUp  float64   `gorm:"column:balance_after_top_up;type:DECIMAL(10,2);not null;default:0"`
-    PaymentDate        time.Time `gorm:"column:payment_date;not null;default:CURRENT_TIMESTAMP"`
+    PaymentID         uint         `gorm:"column:payment_id;primaryKey;autoIncrement"`
+    
+    // FKs
+    UserID            uint         `gorm:"column:user_id;not null"`
+    TransactionID     *uint        `gorm:"column:transaction_id"` // pode ser nulo
+    
+    // Relacionamentos
+    User         *User         `gorm:"foreignKey:UserID;references:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+    Transaction  *Transaction  `gorm:"foreignKey:TransactionID;references:TransactionID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL"`
+    
+    ExternalPaymentID string    `gorm:"column:external_payment_id;type:TEXT;not null"`
+    SupplierName      string    `gorm:"column:supplier_name;type:TEXT;not null"`
+    TopUpValue        float64   `gorm:"column:top_up_value;type:DECIMAL(10,2);not null;default:0"`
+    BalanceAfterTopUp float64   `gorm:"column:balance_after_top_up;type:DECIMAL(10,2);not null;default:0"`
+    PaymentDate       time.Time `gorm:"column:payment_date;not null;default:CURRENT_TIMESTAMP"`
 }
 
 func (Payment) TableName() string {
--- domain/session.go
+++ domain/session.go
@@ -3,10 +3,14 @@ package domain
 import "time"
 
 type Session struct {
-    ID             uint      `gorm:"primaryKey;autoIncrement"`
-    UserID         uint      `gorm:"not null"`
-    AuthMethod     string    `gorm:"type:TEXT;not null"` // "password" ou "google"
-    CreatedAt      time.Time
+    ID        uint   `gorm:"primaryKey;autoIncrement"`
+    UserID    uint   `gorm:"not null"`
+
+    // Relacionamento com User
+    User *User `gorm:"foreignKey:UserID;references:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+
+    AuthMethod string    `gorm:"type:TEXT;not null"` // "password" ou "google"
+    CreatedAt  time.Time
     // Caso queira guardar o token JWT ou refresh_token, adicione campos
     // Token        string
     // ExpiresAt    time.Time

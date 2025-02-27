0acd6bb566f82c1bb16ed10159db38eef876caac
Your Name <filipeborato@hotmail.com>
2025-02-27 16:54:48 +0000

    feat: sql products

---
+++ db/products.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO products (product_name, description, price)
+VALUES
+  ('process', 'price of one process', 0.20),
+  ('process5', 'price of five process', 0.18),
+  ('process10', 'price of ten process', 0.15);
+  
+  SELECT * FROM products;
+
+
+
+
